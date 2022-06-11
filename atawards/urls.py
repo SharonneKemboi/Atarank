331ffc8d425133a08993a852df76e203a70bfab3
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-06-11 22:05:18 +0300

    Add background image

--- atawards/urls.py
+++ atawards/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.hello, name = 'hello'),
+    path('',views.index, name = 'index'),
 ]
