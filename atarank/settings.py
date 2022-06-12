a61ee5e91e7ff35dca875a867a26923094f0bad6
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-06-12 13:26:48 +0300

    Create model profile and Perform migrations

--- atarank/settings.py
+++ atarank/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'atawards',
     'bootstrap4',
+    'cloudinary',
+    'cloudinary_storage',
+    'crispy_forms',
 
 ]
 
@@ -109,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
